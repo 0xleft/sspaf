61b767a9d598c59c28dd9bdb34ec606f7c145c4a
Adomas <107749872+0xleft@users.noreply.github.com>
2023-08-09 10:46:47 +0200

    readme and splash

--- sspaf/sspaf.py
+++ sspaf/sspaf.py
@@ -3,9 +3,36 @@ import argparse
 
 from sspaf.core import render, serve, publish, create
 
+def args_error():
+
+    print("""
+(: SSPAF a superglued single page aooplication framework :)
+
+Author: plusleft
+
+Usage: sspaf <operation> <path>
+
+Operations:
+    render: render the project to the output folder
+    serve: serve the project on port 8080
+    publish: create a production ready version of the project
+    create: initialize the blueprint for a new project
+
+Path:
+    path to the project
+
+Examples:
+    sspaf serve .
+    sspaf publish /home/plusleft/myproject
+    sspaf create /home/plusleft/myproject
+""")
+    
+    exit(1)
+
 def main():
     arguments = argparse.ArgumentParser()
-    arguments.add_argument(type=str, help='render, publish, help', dest='operation')
+    arguments.error = args_error
+    arguments.add_argument(type=str, help='render, publish, create, serve', dest='operation')
     arguments.add_argument(type=str, help='path to the project', dest='path')
 
     args = arguments.parse_args()
